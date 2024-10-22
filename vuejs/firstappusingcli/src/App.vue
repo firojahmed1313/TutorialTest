b1f797060df23e7d398f9f4862d46ec572133fea
MD FIROJ AHMED <74416772+firojahmed1313@users.noreply.github.com>
2024-10-22 21:31:45 +0530

    api ola map typescript vue

---
+++ vuejs/firstappusingcli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<HelloWorld  msg="Welcome to Your Vue.js App" />-->
+    <FirstComp/>
+    <FirstComp/>
+  </div>
+</template>
+
+<script>
+import FirstComp from './components/FirstComp.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+    FirstComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs/firstappusingcli/src/components/FirstComp.vue
@@ -0,0 +1,14 @@
+<template>
+
+<h1>MD FIROJ AHMED</h1>
+</template>
+
+<script>
+export default {
+    name: 'First-d',
+}
+</script>
+
+<style scoped>
+
+</style>
